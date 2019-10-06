786ce75fe3cf6c70cb58d46d05096fdfc585e118
Leigh Halliday <leighhalliday@gmail.com>
2019-10-06 15:57:20 -0400

    Demo react app for graphql

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import "./App.css";
 
-function App() {
+const EVENTS = gql`
+  query EventsQuery {
+    events {
+      id
+      name
+      category {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export default function App() {
+  const { loading, data } = useQuery(EVENTS);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Worst GraphQL Demo Ever</h1>
+
+      <ul>
+        {data.events.map(event => (
+          <li key={event.id}>
+            <h2>{event.name}</h2>
+            <p>Category - {event.category.name}</p>
+          </li>
+        ))}
+      </ul>
+      <AddEvent />
+    </main>
   );
 }
 
-export default App;
+const CATEGORIES = gql`
+  query CategoriesQuery {
+    categories {
+      id
+      name
+    }
+  }
+`;
+
+const CREATE_EVENT = gql`
+  mutation CreateEvent($input: CreateEventInput!) {
+    createEvent(input: $input) {
+      event {
+        id
+      }
+      errors
+    }
+  }
+`;
+
+function AddEvent() {
+  const [categoryId, setCategoryId] = React.useState("");
+  const [name, setName] = React.useState("");
+  const { loading, data } = useQuery(CATEGORIES);
+  const [createEvent] = useMutation(CREATE_EVENT);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <form
+      onSubmit={async event => {
+        event.preventDefault();
+
+        await createEvent({
+          variables: {
+            input: {
+              categoryId,
+              name,
+              startTime: "2019-10-10T10:10:10",
+              endTime: "2019-10-10T11:11:11"
+            }
+          },
+          refetchQueries: [{ query: EVENTS }]
+        });
+
+        setCategoryId("");
+        setName("");
+      }}
+    >
+      <h2>Create an Event</h2>
+
+      <div>
+        <select
+          value={categoryId}
+          onChange={event => {
+            setCategoryId(event.target.value);
+          }}
+          required
+        >
+          <option value="">choose</option>
+          {data.categories.map(category => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={event => {
+            setName(event.target.value);
+          }}
+          placeholder="Event name"
+          required
+        />
+      </div>
+
+      <div>
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://dev-peer-group-graphql.herokuapp.com/graphql"
+});
+
+const ApolloApp = AppComponent => (
+  <ApolloProvider client={client}>
+    <AppComponent />
+  </ApolloProvider>
+);
+
+ReactDOM.render(ApolloApp(App), document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
